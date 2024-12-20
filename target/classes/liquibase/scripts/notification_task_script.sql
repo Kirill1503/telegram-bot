051847b2972e750ecab72dc361268b81203fee76
Kirill1503 <105604464+Kirill1503@users.noreply.github.com>
2024-12-20 18:21:34 +0300

    Second Commit

---
+++ target/classes/liquibase/scripts/notification_task_script.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset kkatyshev:1
+
+CREATE TABLE notification_task (
+    id SERIAL,
+    chat_id INT NOT NULL ,
+    message TEXT NOT NULL,
+    dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+)
